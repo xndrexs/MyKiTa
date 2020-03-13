9ab9fbbbebebcccca0adffc43dc40551e5b77e16
Andreas Pöhler <apoehler@outlook.de>
2020-03-13 15:38:51 +0100

    menu component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,14 @@ import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {DashboardModule} from './view/dashboard/dashboard.module';
 import {LoginModule} from './view/login/login.module';
+import { MenuComponent } from './component/menu/menu.component';
+import { MenuItemComponent } from './component/menu/menu-item/menu-item.component';
 
 @NgModule({
     declarations: [
         AppComponent,
+        MenuComponent,
+        MenuItemComponent,
     ],
     imports: [
         BrowserModule,
