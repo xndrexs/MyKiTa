ab86982c0f4cd636302ff271bfbd834c142f9d4e
Andreas Pöhler <apoehler@outlook.de>
2020-03-13 10:08:07 +0100

    added a few models

---
+++ frontend/src/model/daycare.model.ts
@@ -0,0 +1,6 @@
+import {Address} from "./address.model";
+
+export class DayCare {
+    name: string;
+    address: Address;
+}
---
+++ frontend/src/model/role.enum.ts
@@ -0,0 +1,7 @@
+export enum Role {
+    Child,
+    Parent,
+    User,
+    Manager,
+    Administrator,
+}
